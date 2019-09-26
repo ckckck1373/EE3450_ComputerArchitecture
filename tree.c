ab19d137057f4d3162031771774ceb0c1a833002
Chun Yeh <48740089+ckckck1373@users.noreply.github.com>
2019-09-26 08:18:42 +0800

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,105 +1,163 @@
-// C program for pre-order traversals
+#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
+#include <queue>
+#include <algorithm>
+using namespace std;
 
- int flag = 0;
-
-struct node{
-    int data = -1;
-    struct node *left;
-    struct node *right;
+struct Node{
+    int data;
+    int max;
+    int min;
+    bool state;
+    Node* left;
+    Node* right;
 };
-struct node* node_array[100];
-// crate the new node for tree
-struct node* newNode(int data)//check
-{
-    struct node* node = (struct node*) malloc(sizeof(struct node));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return(node); 
-}
 
-// direct the right place for nodes
-struct node* insert(struct node* node, int info, int k){
-   
-    if(node == NULL){
-        node_array[k] = newNode(info);
-    }else if((flag == 0)&&(node_array[k-1]->right != NULL)) {
-        node_array[k] -> left = insert(node->left, info);
-        flag = 1;
-    }else if((flag == 1)&&(node_array[k-1]->left != NULL)){
-        node_array[k] -> right = insert(node->right, info);
-        flag = 0;
-    }
-    return node;
-}
+//create the new node for the tree
+inline Node* createnode(int value){
+    Node* newnode = new Node;
+    newnode -> data = value;
+    newnode -> max = 0;
+    newnode -> min = 1000;
+    newnode -> state = true;
+    newnode -> left = NULL;
+    newnode -> right  = NULL;
+    return newnode;
+};
 
-// print the pre-order set for the tree
-void printPreorder(struct node* node){
-
+// print the pre-order traversal of the tree
+inline void printPreOrder(Node *node){
     if(node == NULL)
         return;
-    printf("%d ", node->data);
 
-    printPreorder(node->left); 
-    printPreorder(node->right);
+    cout << node->data << " ";
+    printPreOrder(node -> left);
+    printPreOrder(node -> right);
 }
 
-// Driver program to test the function.
-int main(int argc,char** argv){
-    FILE* fp;
+
+// find largst number in tree
+inline int largest(Node* node){
+    if(node == NULL)
+        return 0;
+
+    int left = largest(node -> left);
+    int right = largest(node -> right);
+    if ((node -> data > left) && (node -> data > right)){
+        node -> max = node -> data;
+        return node -> max;
+    }
+    else{
+        node -> max = max(left, right);
+        return node -> max;
+    }
+
+    return node -> max;
+}
+
+
+//find smallest number in tree
+inline int smallest(Node* node){
+    if(node == NULL)
+        return 1000;
+
+    int left = smallest(node -> left);
+    int right = smallest(node -> right);
+    if((node -> data < left) && (node -> data < right)){
+        node -> min = node -> data;
+        return node -> min;
+
+    }
+    else{
+        node -> min = min(left, right);
+        return node -> min;
+    }
+
+    return node -> min;
+}
+
+inline bool checkBSP(Node* node){
+    if(node == NULL)
+        return true;
+
+    Node* node_left = new Node;
+    Node* node_right = new Node;
+    node_left = node -> left;
+    node_right = node -> right;
+
+
+    if(node_left -> max > node -> data  && node_left != NULL){
+        node -> state = false;
+        return node -> state;
+    };
+    if(node_right -> min < node -> data && node_right != NULL){
+        node -> state = false;
+        return node -> state;
+    };
+
+    //cout << node_left-> state << endl << node_right-> state;
+    return ((node_left-> state) && (node_right -> state));
+
+}
+int main()
+{
     int num_node = 0;
     int node_temp = 0;
-    int data_base[100];
-    int i = 0;
-   
-    
+    bool property = true;
 
-    //讀檔案的格式，之後可能會用到
-    /*
-    fp = fopen(argv[1],"r");
-    if(fp == NULL){
-        printf("open failure");
-        return 1;
-    }else{
-        num_node = fscanf(fp, "%d", &num_node); //先讀一個有多少個node
-        while(fscanf(fp, "%d%*c", &node_temp) != EOF){ //有c但是略過c(好玩噎))
-            //printf("%d ", node_temp); for testing I/O
-            data_base[i] = node_temp;
-            i += 1;
+    // for input data
+    scanf("%d" ,&num_node);
+    int data_base[1000];
+    queue <int*> data_baseq;
+
+
+    for(int k = 0;k < num_node;k++){
+        scanf("%d%*c", &node_temp);
+        data_base[k] = node_temp;
+        data_baseq.push(&data_base[k]);
+    }
+
+
+    Node* root = createnode(*data_baseq.front());
+
+    data_baseq.pop();
+    queue <Node*> nodes;
+    nodes.push(root);
+
+    while(data_baseq.empty() == false){
+        Node *newnode = nodes.front();
+        int temp_left;
+        int temp_right = -1;
+        if(newnode -> left == NULL){
+            newnode -> left = createnode(*data_baseq.front());
+            temp_left = *data_baseq.front();
+            data_baseq.pop();
+            nodes.push(newnode -> left);
         }
-        
-    }
-    fclose(fp);
-    */
+        if((newnode -> right == NULL) &&(data_baseq.empty() == false)){
+            newnode -> right = createnode(*data_baseq.front());
 
-    //for input data
-    scanf("%d ", &num_node);
-    while(scanf("%d%*c", &node_temp) != EOF){
-        data_base[i] = node_temp;
-        i += 1;
-    }
-
-    struct node* root = newNode(data_base[0]); 
-
-    for(int k = 1; k < num_node; ++k){
-        insert(root, data_base[k], node_array[k], k);
+            temp_right = *data_baseq.front();
+            data_baseq.pop();
+            nodes.push(newnode -> right);
+        }
+        property = ((temp_left <= nodes.front()->data) && ((nodes.front() -> data <= temp_right) || (temp_right == -1)))? property : false;
+        nodes.pop();
     }
 
-    
-/*
-    root -> left      = newNode(data_base[1]);
-    root -> right     = newNode(data_base[2]);
-    root -> left -> left = newNode(data_base[3]);
-    root -> left -> right = newNode(data_base[4]);
-    root -> right -> left = newNode(data_base[5]);
-    root -> right -> right = newNode(data_base[6]);
-    //要general
-*/
-    printPreorder(root);
+    int temp = largest(root);
+    int temp2 = smallest(root);
+    bool check = checkBSP(root)&checkBSP(root -> left);//**check不應該這樣寫
+
+    printPreOrder(root);
+
+
+    if(check&&property){
+        printf("\ntrue\n");
+    }else{
+        printf("\nfalse\n");
+    }
     return 0;
 
-
-
 }
+
